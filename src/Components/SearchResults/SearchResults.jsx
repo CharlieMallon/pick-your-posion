d5af1d489b9e08c5f6a7c32a11b7e5aefd8b30d1
CharlieMallon <penguin47@hotmail.co.uk>
2022-04-18 17:36:39 +0100

    added the search results function

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -1,10 +1,34 @@
-import React from "react"
-import styles from "./SearchResults.module.css"
 
-const SearchResults = () => {
+import React, { useMemo } from "react";
+import { useQuery } from "react-query";
+import { getCocktails } from "../../Scripts/network-functions";
+import ResultCard from "../ResultCard/ResultCard";
+import styles from './SearchResults.module.css';
+
+const SearchResults = ({ searchFor }) => {
+    // gets list of cocktails with the ingredient that we searchedFor
+    const { data, error } = useQuery(["drink search", searchFor], () =>
+        getCocktails(searchFor)
+    );
+    // remembers the last thing searched, if its the same gives the same set of data
+    const cocktails = useMemo(() => {
+        // if no data return
+        if (!data) return;
+        return data.drinks;
+    }, [data]);
+    // show the list of cocktails
+    console.log('cocktails', cocktails)
+    // if there is an error, show it
+    if (error) return <h2>an unknown error has occurred</h2>;
+
     return (
-        <div className={styles.main}>SearchResults</div>
-    )
-}
+        <div className={styles.main}>
+            {/* only map through cocktails if they exist */}
+            {cocktails && cocktails.map((it) => (
+                <ResultCard card={it} key={it.idDrink} />
+            ))}
+        </div>
+    );
+};
 
-export default SearchResults
+export default SearchResults;

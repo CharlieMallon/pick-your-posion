a22f4c34fb0c62644d09278a09abcc0dcfefcf6a
CharlieMallon <penguin47@hotmail.co.uk>
2022-04-18 17:00:51 +0100

    added drop down list for ingrediant search

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import { Routes, Route, HashRouter } from "react-router-dom";
 import SearchPage from "./Pages/SearchPage/SearchPage";
+import { QueryClient, QueryClientProvider} from 'react-query'
 
+const queryClient = new QueryClient();
 function App() {
 	return (
 		<HashRouter>
-			<div className="App">
-				<NavBar/>
-				<Routes>
-					<Route path="/" element={<SearchPage/>}/>
-				</Routes> 
-			</div>
+			<QueryClientProvider client={queryClient} >
+				<div className="App">
+					<NavBar/>
+					<Routes>
+						<Route path="/" element={<SearchPage/>}/>
+					</Routes> 
+				</div>
+			</QueryClientProvider>
 		</HashRouter>
 	);
 }
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,15 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
+import { useQuery } from 'react-query';
+import Select from 'react-select';
+import { getIngredients } from '../../Scripts/network-functions';
 import styles from './SearchBar.module.css'
 
 const SearchBar = ({setSearchTerm}) => {
     const [inputValue, setInputValue] = useState({ Label: "", Value: ""});
 
+    const { data, error } = useQuery("ingredients", getIngredients);
+
+    // get the list of ingredients and make them look pretty for the select box
+    const ingredients = useMemo(() => {
+        if (!data) return;
+        return data.drinks
+            .map((it) => ({
+                value: it.strIngredient1,
+                label: it.strIngredient1,
+            }))
+            .sort((a, b) =>
+                a.label > b.label ? 1 : b.label > a.label ? -1 : 0
+            );
+        }, [data]);
+
     const handleSearchClick = () => {
         setSearchTerm(inputValue.value);
     }
     
+    if (error) return <h6>there has been an error, oops</h6>;
+
     return (
-        <div className = {styles.main}>SearchBar</div>
+        <div className = {styles.main}>
+            <div className={styles.searchInput} >
+                <Select
+                    value={inputValue}
+                    name="search"
+                    options={ingredients}
+                    onChange={(e) => setInputValue(e ?? { label: "", value: "" })}
+                />
+            </div>
+            <button className={styles.searchButton} onClick={handleSearchClick}>
+                Search
+            </button>
+        </div>
     )
 }
 

389d5854a82c7e72e9ea3133fc450d0e2a1cf9ae
CharlieMallon <penguin47@hotmail.co.uk>
2022-04-18 17:57:50 +0100

    added recipe page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import { Routes, Route, HashRouter } from "react-router-dom";
 import SearchPage from "./Pages/SearchPage/SearchPage";
+import RecipePage from "./Pages/RecipePage/recipePage";
 import { QueryClient, QueryClientProvider} from 'react-query'
 
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ function App() {
 					<NavBar/>
 					<Routes>
 						<Route path="/" element={<SearchPage/>}/>
+						<Route path="/recipe/:drinkId" element={<RecipePage/>}/>
 					</Routes> 
 				</div>
 			</QueryClientProvider>
